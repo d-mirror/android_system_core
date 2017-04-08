c2ae1174507d887c10f71da15f492101ee7800fc
Alex Naidis <alex.naidis@linux.com>
2017-04-09 01:29:27 +0200

    init: Weaken property override security for the init extension
    Sometimes we need to override ro.* properties by using our vendor init
    extension.
    
    Previously there was a security check which was blocking that.
    To resolve the issue, we need to weaken the security check during the
    execution of our vendor init extension.
    
    This is safe because the vendor init extension gets executed as part of init
    construction and it is considered a trusted system component.
    
    Change-Id: I6198b453745cb92c65d3e3d49e3262354cddd2a2
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: 15aafc0c179544dd173a7afdce8d4d24b19219e7
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: d5ce44bfdf41cbe51f7432a04409bb11147b35a4
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: ff0763ca639d4987d1130f9838474b935f9b9950
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: 760b30eff74a6e212f57b09be8505ae8283ef3c8
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: 259b97b043c0769d4c8886c9ace9686ca2188536
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1204,6 +1204,9 @@ void PropertyLoadBootDefaults() {
         }
     }
 
+    // Weaken property override security during execution of the vendor init extension
+    weaken_prop_override_security = true;
+
     // Update with vendor-specific property runtime overrides
     vendor_load_properties();
 
@@ -1212,6 +1215,9 @@ void PropertyLoadBootDefaults() {
     property_derive_build_fingerprint();
     property_derive_legacy_build_fingerprint();
     property_initialize_ro_cpu_abilist();
+
+    // Restore the normal property override security after init extension is executed
+    weaken_prop_override_security = false;
 
     update_sys_usb_config();
 
